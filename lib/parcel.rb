86e72e6da617cec42bfabd5dc23e4b71322a87bd
Willem Larsen <willemlarsen@gmail.com>
2014-07-14 14:19:59 -0700

    cleaned up use of .to_i

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,18 +1,18 @@
 class Parcel
 	def initialize(height, width, length, weight)
-		@height = height
-		@width = width
-		@length = length
-		@weight = weight
+		@height = height.to_i
+		@width = width.to_i
+		@length = length.to_i
+		@weight = weight.to_i
 	end
 	def dimensions
-		@height.to_i * @width.to_i * @length.to_i
+		@height * @width * @length
 	end
 	def cost_to_ship
 		if self.dimensions <= 10
-			1 * @weight.to_i
+			1 * @weight
 		elsif self.dimensions <= 20
-			2 * @weight.to_i
+			2 * @weight
 		else
 			false
 		end
